b9745d77d8676ac1535c08fce991ac12551d3a96
David Stoyanov <david_st1@abv.bg>
2019-04-01 22:30:26 +0300

    guldfile changed, docs recursion fixed

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,9 @@ const GENERATED_NAME_FOLDER = "docs"; //_site
 
 let gulp = require("gulp"),
 	autoprefixer = require("gulp-autoprefixer"),
-	browserSync = require('browser-sync').create()
-;
+	exec = require("gulp-exec"),
+	browserSync = require('browser-sync').create(),
+	cp = require("child_process");
 
 gulp.task("css", function() {
 	return gulp.src( '_assets/css/**/*.css' )
@@ -12,6 +13,12 @@ gulp.task("css", function() {
 		.pipe( gulp.dest( './' + GENERATED_NAME_FOLDER + '/css/' ) )
 		.pipe(browserSync.stream({ match: '**/*.css' }))
 	;
+});
+
+// Jekyll
+gulp.task("jekyll", function() {
+	// return cp.spawn("bundle", ["exec", "jekyll", "build"], { stdio: "inherit" });
+	return cp.spawn("jekyll.bat", ["build"], { stdio: "inherit" });
 });
 
 gulp.task("watch", function() {
@@ -24,8 +31,17 @@ gulp.task("watch", function() {
 
 	gulp.watch( '_assets/css/**/*.css', gulp.series('css') );
 
+	gulp.watch(
+		[
+			"./*.html",
+			"./_includes/*.html",
+			"./_layouts/*.html",
+			"./_posts/**/*.*"
+		]
+	).on('change', gulp.series('jekyll', 'css'));
+
 	gulp.watch( GENERATED_NAME_FOLDER + '/**/*.html' ).on('change', browserSync.reload );
 	gulp.watch( GENERATED_NAME_FOLDER + '/**/*.js' ).on('change', browserSync.reload );
 });
 
-gulp.task("default", gulp.series("watch"));
+gulp.task("default", gulp.series("jekyll", "css", "watch"));
